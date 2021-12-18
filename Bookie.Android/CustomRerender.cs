a27fbefddb9b84a757ece9f36f5654c9acbb07e5
ElseVn <67472527+UIT-19521292@users.noreply.github.com>
2021-12-18 20:05:56 +0700

    add login and reg view

---
+++ Bookie.Android/CustomRerender.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Bookie.Custom;
+using Bookie.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(myEntry), typeof(MyEntryRenderer))]
+[assembly: ExportRenderer(typeof(myButton), typeof(MyButtonRenderer))]
+namespace Bookie.Droid
+{
+    public class MyButtonRenderer : ButtonRenderer
+    {
+        public MyButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            var button = this.Control;
+            button.SetAllCaps(false);
+        }
+    }
+    public class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                //Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(10f);
+                gradientDrawable.SetStroke(5, Android.Graphics.Color.Black);
+                gradientDrawable.SetColor(Android.Graphics.Color.White);
+                Control.SetBackground(gradientDrawable);
+                Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight, Control.PaddingBottom);
+            }
+        }
+    }
+}
---
+++ Bookie/Custom/myCustom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Bookie.Custom
+{
+    public class myButton:Button
+    {
+    }
+    public class myEntry : Entry
+    {
+    }
+}
